7e690d41352a85b7c4f76617320adcb5f26813ed
Rineet-Samal <165139314+Rineet-Samal@users.noreply.github.com>
2026-09-16 19:08:08 +0530

    feat: cosmetic changes

--- firebase-config.js
+++ firebase-config.js
@@ -16,9 +16,6 @@
 // Note: "allow read, write: if true" means anyone with your config can read/write.
 // That's fine for a private family list, but never put real secrets in Firestore this way.
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
